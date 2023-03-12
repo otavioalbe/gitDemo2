8943f68272604be0cde3cd4c4f131a3852ebdb2b
Otávio Albuquerque <otavioalbe@gmail.com>
2023-03-12 19:19:12 -0300

    5.0 econo

--- src/App.java
+++ src/App.java
@@ -17,21 +17,53 @@ public class App {
         MotorBasico motorSUV = new MotorBasico(TipoCombustivel.GASOLINA, 8);
         TanqueCombustivel tanqueSUV = new TanqueCombustivel(TipoCombustivel.FLEX, 55);
         Carro suv = new Carro("SUV", motorSUV, tanqueSUV);
-
+        // carro SUVFlex
+        TanqueCombustivelFlex tanqueFlex = new TanqueCombustivelFlex(65);
+        MotorBasico motorSUVFlex = new MotorFlex(8,6,tanqueFlex);
+        Carro suvFlex = new Carro("SUVFlex", motorSUVFlex, tanqueFlex);
+        // carro econo
+        MotorBasico motorEcono = new MotorEcono();
+        TanqueCombustivel tanqueEcono = new TanqueCombustivel(TipoCombustivel.GASOLINA, 55);
+        Carro econo = new Carro("Econo", motorEcono, tanqueEcono);
 
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
         System.out.println(esportivo);
         System.out.println(utilitario);
         System.out.println(suv);
+        System.out.println(suvFlex);
+        System.out.println(econo);
 
-        System.out.println("\n\n----------------");
-        System.out.println("\nAbastencendo carro basico com gasolina");
+        System.out.println("\n\n--------------------------------");
+        System.out.println("\nAbastencendo todos os carros,,");
         basico.abastece(TipoCombustivel.GASOLINA, 55);
+        esportivo.abastece(TipoCombustivel.GASOLINA, 40);
+        utilitario.abastece(TipoCombustivel.DIESEL, 60);
+        suv.abastece(TipoCombustivel.GASOLINA, 50);
+        suvFlex.abastece(TipoCombustivel.ALCOOL, 20);
+        suvFlex.abastece(TipoCombustivel.GASOLINA, 40);
+        econo.abastece(TipoCombustivel.GASOLINA, 30);
+
         System.out.println(basico);
-        System.out.println("\nViajando com o carro basico");
-        basico.viaja(250);
+        System.out.println(esportivo);
+        System.out.println(utilitario);
+        System.out.println(suv);
+        System.out.println(suvFlex);
+        System.out.println(econo);
+        
         basico.viaja(150);
+        esportivo.viaja(100);
+        utilitario.viaja(200);
+        suv.viaja(120);
+        suvFlex.viaja(150);
+        econo.viaja(200);
+
         System.out.println(basico);
+        System.out.println(esportivo);
+        System.out.println(utilitario);
+        System.out.println(suv);
+        System.out.println(suvFlex);
+        System.out.println(econo);
+
     }
 }
