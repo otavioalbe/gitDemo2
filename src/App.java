b9799d2591e41f939d8b603e3df426d6b8ab13d9
Otávio Albuquerque <otavioalbe@gmail.com>
2023-03-12 18:33:10 -0300

    3.0 utilitario

--- src/App.java
+++ src/App.java
@@ -3,9 +3,12 @@ public class App {
 
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
         Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
+        Carro utilitario = new Carro("Utilitário", TipoCombustivel.DIESEL, 5, 70);
 
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
+        System.out.println(esportivo);
+        System.out.println(utilitario);
 
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro basico com gasolina");
